ab0305cfe5ed10a6937948046baf059d35e81732
Terry vanker <Terry.vanker89@gmail.com>
2024-02-27 09:36:27 +0100

    update

--- blog/src/Navbar.jsx
+++ blog/src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>The New Blog</h1>
       <div className="links">
-        <Link to="/">Home</Link>
+        <Link to="/">Home </Link>
         <Link to="/create">New blog</Link>
       </div>
     </nav>
